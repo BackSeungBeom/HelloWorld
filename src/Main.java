c97f0f0c0774059bb6fc6a818a6be366a7874b3b
Back SeungBeom <seungbeom1234@handong.ac.kr>
2023-09-02 12:40:48 +0900

    깃커밋테스트

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Hello World!!!");
         System.out.print("이름 입력: ");
         String name = in.next();
-        System.out.printf("[%s]님 환영합니다!", name);
+        System.out.printf("[%s]님 환영합니다!!!!!!!!", name);
     }
 }
